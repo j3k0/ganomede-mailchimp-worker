8b07ecca9de45e4210892570f4685b457394410d
Alexey Zabrodsky <elmigranto@gmail.com>
2017-03-18 17:33:34 +0500

    Added API clients for mailchimp and usermeta.

---
+++ src/apis/MailchimpClient.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const urlEscape = require('url-escape-tag');
+const BaseClient = require('./BaseClient');
+const {MailchimpPayload} = require('../objects');
+
+const toBase64 = (str) => Buffer.from(str, 'utf8').toString('base64');
+
+class MailchimpClient extends BaseClient {
+  constructor ({clientId, apiKey, dataCenter}) {
+    super(`https://${dataCenter}.api.mailchimp.com/3.0`, {
+      headers: {'authorization': `Basic ${toBase64(`${clientId}:${apiKey}`)}`}
+    });
+  }
+
+  subscribe (listId, request, callback) {
+    const payload = new MailchimpPayload(request);
+    this.apiCall('post', urlEscape`/lists/${listId}/members`, payload, callback);
+  }
+}
+
+module.exports = MailchimpClient;
---
+++ tests/apis/MailchimpClient.test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const url = require('url');
+const {SubscriptionRequest} = require('../../src/objects');
+const MailchimpClient = require('../../src/apis/MailchimpClient');
+
+describe('MailchimpClient', () => {
+  const client = new MailchimpClient({
+    clientId: 'ganomede-mailchimp-worker',
+    dataCenter: 'us01',
+    apiKey: 'secret'
+  });
+
+  describe('new MailchimpClient()', () => {
+    it('correctly sets up base url', () => {
+      expect(client.pathPrefix).to.eql('/3.0');
+      expect(client.api.url).to.eql(url.parse('https://us01.api.mailchimp.com'));
+    });
+
+    it('sets up auth header', () => {
+      expect(client.api.headers).to.have.property('authorization');
+      expect(client.api.headers.authorization).to.match(/^Basic /);
+    });
+  });
+
+  describe('#subscribe()', () => {
+    it('uses subscription request to subscribe user to listId', (done) => {
+      const req = new SubscriptionRequest({
+        userId: 'bob',
+        email: 'bob@example.com',
+        from: 'some-app',
+        metadata: {}
+      });
+
+      td.replace(client.api, 'post', td.function());
+
+      td.when(client.api.post('/3.0/lists/deadbeef/members', td.matchers.anything(), td.callback))
+        .thenCallback(null, {}, {}, {ok: true});
+
+      client.subscribe('deadbeef', req, (err, reply) => {
+        expect(err).to.be.null;
+        expect(reply).to.eql({ok: true});
+        done();
+      });
+    });
+  });
+});
