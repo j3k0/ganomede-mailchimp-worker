693a91ae34311ade2f427b58807ee13823b021a3
Alexey Zabrodsky <elmigranto@gmail.com>
2017-03-20 15:34:38 +0500

    Logger prints errors via `debugInspect`.

--- src/logger.js
+++ src/logger.js
@@ -2,8 +2,13 @@
 
 const bunyan = require('bunyan');
 const config = require('../config');
+const {debugInspect} = require('./utils');
 
 module.exports = bunyan.createLogger({
   level: config.logLevel,
-  name: config.name
+  name: config.name,
+  serializers: {
+    err: debugInspect,
+    error: debugInspect
+  }
 });
