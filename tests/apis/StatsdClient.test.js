c7687bd3a9e11602ff9d75bc085bea4eaca897df
Alexey Zabrodsky <elmigranto@gmail.com>
2017-03-24 02:03:50 +0500

    Fixed tests for StatsD warning.

--- tests/apis/StatsdClient.test.js
+++ tests/apis/StatsdClient.test.js
@@ -19,10 +19,11 @@ describe('StatsdClient', () => {
     it('creates mock client on missing config and warns about it', () => {
       expect(new StatsdClient().mock).to.be.true;
       td.verify(loggerChild.warn({
-        hostname: undefined,
+        host: undefined,
         port: undefined,
-        prefix: undefined
-      }, 'StatsdClient missing some config options'));
+        prefix: undefined,
+        mock: true
+      }, 'StatsdClient missing some config options, stats will not be sent.'));
     });
   });
 });
